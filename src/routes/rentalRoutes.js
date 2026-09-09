8a75c751ec9e41c23760da52efa434dd1798cd8e
Quý Phan <phannguyenngocquy4339@gmail.com>
2026-09-09 12:59:25 +0700

    Delete src directory

--- src/routes/rentalRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const rentalController = require('../controllers/rentalController');
-
-// Đảm bảo trong rentalController có các hàm này
-router.get('/', rentalController.getAllRentals);
-router.post('/', rentalController.createRental);
-
-module.exports = router;
