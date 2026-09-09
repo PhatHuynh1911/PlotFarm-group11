4b4c20d8d2df070b111d2adb84c369efc8cba6d3
PhatHuynh1911 <phatht2018@gmail.com>
2026-09-09 17:08:02 +0700

    update UI AdminPage

--- BE/src/server.js
+++ BE/src/server.js
@@ -9,7 +9,7 @@ const { connectDB } = require('./config/db');
 const plotRoutes = require('./routes/plotRoutes');
 const rentalRoutes = require('./routes/rentalRoutes');
 const authRoutes = require('./routes/authRoutes');
-const { getAdminDashboard } = require('./controllers/authController');
+const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -26,7 +26,7 @@ connectDB();
 app.use('/api/plots', plotRoutes);
 app.use('/api/rentals', rentalRoutes);
 app.use('/api/auth', authRoutes);
-app.get('/api/admin/dashboard', getAdminDashboard);
+app.use('/api/admin', adminRoutes);
 
 // Route kiểm tra server
 app.get('/', (req, res) => {
